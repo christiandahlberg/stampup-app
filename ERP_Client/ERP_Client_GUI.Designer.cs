637afee4db47dee87e770cf01c40d850768fc1fe
Christian Dahlberg <ch7040da-s@student.lu.se>
2019-03-04 14:36:20 +0100

    ERP Main Client GUI

--- ERP_Client/ERP_Client_GUI.Designer.cs
+++ ERP_Client/ERP_Client_GUI.Designer.cs
@@ -29,47 +29,66 @@
         private void InitializeComponent()
         {
             this.label1 = new System.Windows.Forms.Label();
-            this.button1 = new System.Windows.Forms.Button();
+            this.btn_JavaClient = new System.Windows.Forms.Button();
             this.button2 = new System.Windows.Forms.Button();
             this.SuspendLayout();
             // 
             // label1
             // 
             this.label1.AutoSize = true;
-            this.label1.Location = new System.Drawing.Point(198, 46);
+            this.label1.Font = new System.Drawing.Font("Arial Narrow", 10.2F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.Location = new System.Drawing.Point(252, 33);
+            this.label1.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
             this.label1.Name = "label1";
-            this.label1.Size = new System.Drawing.Size(71, 13);
+            this.label1.Size = new System.Drawing.Size(105, 22);
             this.label1.TabIndex = 0;
             this.label1.Text = "Choose client";
             // 
-            // button1
+            // btn_JavaClient
             // 
-            this.button1.Location = new System.Drawing.Point(52, 166);
-            this.button1.Name = "button1";
-            this.button1.Size = new System.Drawing.Size(365, 23);
-            this.button1.TabIndex = 1;
-            this.button1.Text = "Java Client";
-            this.button1.UseVisualStyleBackColor = true;
-            this.button1.Click += new System.EventHandler(this.button1_Click);
+            this.btn_JavaClient.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(255)))), ((int)(((byte)(128)))), ((int)(((byte)(0)))));
+            this.btn_JavaClient.FlatAppearance.BorderColor = System.Drawing.Color.FromArgb(((int)(((byte)(0)))), ((int)(((byte)(255)))), ((int)(((byte)(255)))), ((int)(((byte)(255)))));
+            this.btn_JavaClient.FlatAppearance.BorderSize = 0;
+            this.btn_JavaClient.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.btn_JavaClient.Font = new System.Drawing.Font("Microsoft Sans Serif", 7.8F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btn_JavaClient.ForeColor = System.Drawing.Color.White;
+            this.btn_JavaClient.Location = new System.Drawing.Point(65, 130);
+            this.btn_JavaClient.Margin = new System.Windows.Forms.Padding(4, 2, 4, 2);
+            this.btn_JavaClient.Name = "btn_JavaClient";
+            this.btn_JavaClient.Size = new System.Drawing.Size(487, 36);
+            this.btn_JavaClient.TabIndex = 0;
+            this.btn_JavaClient.TabStop = false;
+            this.btn_JavaClient.Text = "Java Client";
+            this.btn_JavaClient.UseVisualStyleBackColor = false;
+            this.btn_JavaClient.Click += new System.EventHandler(this.button1_Click);
             // 
             // button2
             // 
-            this.button2.Location = new System.Drawing.Point(52, 117);
+            this.button2.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(255)))), ((int)(((byte)(128)))), ((int)(((byte)(0)))));
+            this.button2.FlatAppearance.BorderColor = System.Drawing.Color.FromArgb(((int)(((byte)(0)))), ((int)(((byte)(255)))), ((int)(((byte)(255)))), ((int)(((byte)(255)))));
+            this.button2.FlatAppearance.BorderSize = 0;
+            this.button2.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.button2.Font = new System.Drawing.Font("Microsoft Sans Serif", 7.8F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.button2.ForeColor = System.Drawing.Color.White;
+            this.button2.Location = new System.Drawing.Point(65, 76);
+            this.button2.Margin = new System.Windows.Forms.Padding(4, 2, 4, 2);
             this.button2.Name = "button2";
-            this.button2.Size = new System.Drawing.Size(365, 23);
-            this.button2.TabIndex = 2;
+            this.button2.Size = new System.Drawing.Size(487, 36);
+            this.button2.TabIndex = 0;
+            this.button2.TabStop = false;
             this.button2.Text = "Windows Forms Client";
-            this.button2.UseVisualStyleBackColor = true;
+            this.button2.UseVisualStyleBackColor = false;
             this.button2.Click += new System.EventHandler(this.button2_Click);
             // 
             // ERP_Client_GUI
             // 
-            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(470, 299);
+            this.ClientSize = new System.Drawing.Size(627, 211);
             this.Controls.Add(this.button2);
-            this.Controls.Add(this.button1);
+            this.Controls.Add(this.btn_JavaClient);
             this.Controls.Add(this.label1);
+            this.Margin = new System.Windows.Forms.Padding(4);
             this.Name = "ERP_Client_GUI";
             this.Text = "ERP Client";
             this.ResumeLayout(false);
@@ -80,7 +99,7 @@
         #endregion
 
         private System.Windows.Forms.Label label1;
-        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Button btn_JavaClient;
         private System.Windows.Forms.Button button2;
     }
 }
