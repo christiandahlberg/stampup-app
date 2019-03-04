9ed82ce71aae174f5a4f39f36c0918f2d97302b3
jpetersson26 <jpetersson26@gmail.com>
2019-03-04 10:18:03 +0100

    WS1

---
+++ WS2_WebService/GetEntities.asmx
@@ -0,0 +1,1 @@
+﻿<%@ WebService Language="C#" CodeBehind="GetEntities.asmx.cs" Class="WS2_WebService.WebService1" %>
---
+++ WS2_WebService/GetEntities.asmx.cs
@@ -0,0 +1,73 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Services;
+
+
+
+namespace WS2_WebService
+{
+    /// <summary>
+    /// Summary description for WebService1
+    /// </summary>
+    [WebService(Namespace = "http://tempuri.org/")]
+    [WebServiceBinding(ConformsTo = WsiProfiles.BasicProfile1_1)]
+    [System.ComponentModel.ToolboxItem(false)]
+    // To allow this Web Service to be called from script, using ASP.NET AJAX, uncomment the following line. 
+    // [System.Web.Script.Services.ScriptService]
+    public class WebService1 : System.Web.Services.WebService
+    {
+
+        /// <summary>
+        /// Summary description for GetEntities
+        /// </summary>
+        [WebService(Namespace = "http://tempuri.org/")]
+        [WebServiceBinding(ConformsTo = WsiProfiles.BasicProfile1_1)]
+        [System.ComponentModel.ToolboxItem(false)]
+        // To allow this Web Service to be called from script, using ASP.NET AJAX, uncomment the following line. 
+        // [System.Web.Script.Services.ScriptService]
+        public class GetEntities : System.Web.Services.WebService
+        {
+
+            [WebMethod]
+            public List<Customer> GetCustomers()
+            {
+
+                CustomerDAL customerDAL = new CustomerDAL();
+                List<Customer> customerList = customerDAL.GetAllCustomers();
+
+                return customerList;
+            }
+
+            [WebMethod]
+            public List<Store> GetStores()
+            {
+
+                StoreDAL storeDAL = new StoreDAL();
+                List<Store> storeList = storeDAL.GetAllStores();
+
+                return storeList;
+            }
+
+            [WebMethod]
+            public List<Offer> GetOffers()
+            {
+
+                OfferDAL offerDAL = new OfferDAL();
+                List<Offer> offerList = offerDAL.GetAllOffers();
+
+                return offerList;
+            }
+
+            [WebMethod]
+            public List<Subscription> GetSubscriptions()
+            {
+                OfferDAL subscriptionDAL = new OfferDAL();
+                List<Subscription> subscriptionList = subscriptionDAL.GetSubscriptions();
+
+                return subscriptionList;
+            }
+        }
+    }
+}
