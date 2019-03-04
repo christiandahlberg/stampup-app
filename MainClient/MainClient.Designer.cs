5a6895270dd33429e0ac56dd1b84cd700b3bc72e
Christian Dahlberg <ch7040da-s@student.lu.se>
2019-03-04 13:55:30 +0100

    MainClient semantics update

--- MainClient/MainClient.Designer.cs
+++ MainClient/MainClient.Designer.cs
@@ -38,76 +38,119 @@
             // 
             // btnPK1
             // 
-            this.btnPK1.Location = new System.Drawing.Point(30, 96);
+            this.btnPK1.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(255)))), ((int)(((byte)(128)))), ((int)(((byte)(0)))));
+            this.btnPK1.FlatAppearance.BorderColor = System.Drawing.Color.FromArgb(((int)(((byte)(0)))), ((int)(((byte)(255)))), ((int)(((byte)(255)))), ((int)(((byte)(255)))));
+            this.btnPK1.FlatAppearance.BorderSize = 0;
+            this.btnPK1.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.btnPK1.Font = new System.Drawing.Font("Microsoft Sans Serif", 7.8F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnPK1.ForeColor = System.Drawing.Color.White;
+            this.btnPK1.Location = new System.Drawing.Point(40, 110);
+            this.btnPK1.Margin = new System.Windows.Forms.Padding(4, 2, 4, 2);
             this.btnPK1.Name = "btnPK1";
-            this.btnPK1.Size = new System.Drawing.Size(492, 23);
+            this.btnPK1.Size = new System.Drawing.Size(656, 38);
             this.btnPK1.TabIndex = 0;
+            this.btnPK1.TabStop = false;
             this.btnPK1.Text = "Programkonstruktion 1";
-            this.btnPK1.UseVisualStyleBackColor = true;
+            this.btnPK1.UseVisualStyleBackColor = false;
             this.btnPK1.Click += new System.EventHandler(this.btnPK1_Click);
             // 
             // btnPK2
             // 
-            this.btnPK2.Location = new System.Drawing.Point(30, 134);
+            this.btnPK2.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(255)))), ((int)(((byte)(128)))), ((int)(((byte)(0)))));
+            this.btnPK2.FlatAppearance.BorderColor = System.Drawing.Color.FromArgb(((int)(((byte)(0)))), ((int)(((byte)(255)))), ((int)(((byte)(255)))), ((int)(((byte)(255)))));
+            this.btnPK2.FlatAppearance.BorderSize = 0;
+            this.btnPK2.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.btnPK2.Font = new System.Drawing.Font("Microsoft Sans Serif", 7.8F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnPK2.ForeColor = System.Drawing.Color.White;
+            this.btnPK2.Location = new System.Drawing.Point(40, 163);
+            this.btnPK2.Margin = new System.Windows.Forms.Padding(4, 2, 4, 2);
             this.btnPK2.Name = "btnPK2";
-            this.btnPK2.Size = new System.Drawing.Size(492, 23);
-            this.btnPK2.TabIndex = 1;
+            this.btnPK2.Size = new System.Drawing.Size(656, 38);
+            this.btnPK2.TabIndex = 0;
+            this.btnPK2.TabStop = false;
             this.btnPK2.Text = "Programkonstruktion 2";
-            this.btnPK2.UseVisualStyleBackColor = true;
+            this.btnPK2.UseVisualStyleBackColor = false;
             this.btnPK2.Click += new System.EventHandler(this.btnPK2_Click);
             // 
             // btnWS1
             // 
-            this.btnWS1.Location = new System.Drawing.Point(30, 183);
+            this.btnWS1.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(255)))), ((int)(((byte)(128)))), ((int)(((byte)(0)))));
+            this.btnWS1.FlatAppearance.BorderColor = System.Drawing.Color.FromArgb(((int)(((byte)(0)))), ((int)(((byte)(255)))), ((int)(((byte)(255)))), ((int)(((byte)(255)))));
+            this.btnWS1.FlatAppearance.BorderSize = 0;
+            this.btnWS1.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.btnWS1.Font = new System.Drawing.Font("Microsoft Sans Serif", 7.8F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnWS1.ForeColor = System.Drawing.Color.White;
+            this.btnWS1.Location = new System.Drawing.Point(40, 216);
+            this.btnWS1.Margin = new System.Windows.Forms.Padding(4, 2, 4, 2);
             this.btnWS1.Name = "btnWS1";
-            this.btnWS1.Size = new System.Drawing.Size(492, 23);
-            this.btnWS1.TabIndex = 2;
+            this.btnWS1.Size = new System.Drawing.Size(656, 38);
+            this.btnWS1.TabIndex = 0;
+            this.btnWS1.TabStop = false;
             this.btnWS1.Text = "Web Service 1";
-            this.btnWS1.UseVisualStyleBackColor = true;
+            this.btnWS1.UseVisualStyleBackColor = false;
             this.btnWS1.Click += new System.EventHandler(this.btnWS1_Click);
             // 
             // btnWS2
             // 
-            this.btnWS2.Location = new System.Drawing.Point(30, 221);
+            this.btnWS2.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(255)))), ((int)(((byte)(128)))), ((int)(((byte)(0)))));
+            this.btnWS2.FlatAppearance.BorderColor = System.Drawing.Color.FromArgb(((int)(((byte)(0)))), ((int)(((byte)(255)))), ((int)(((byte)(255)))), ((int)(((byte)(255)))));
+            this.btnWS2.FlatAppearance.BorderSize = 0;
+            this.btnWS2.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.btnWS2.Font = new System.Drawing.Font("Microsoft Sans Serif", 7.8F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnWS2.ForeColor = System.Drawing.Color.White;
+            this.btnWS2.Location = new System.Drawing.Point(40, 268);
+            this.btnWS2.Margin = new System.Windows.Forms.Padding(4, 2, 4, 2);
             this.btnWS2.Name = "btnWS2";
-            this.btnWS2.Size = new System.Drawing.Size(492, 23);
-            this.btnWS2.TabIndex = 3;
+            this.btnWS2.Size = new System.Drawing.Size(656, 38);
+            this.btnWS2.TabIndex = 0;
+            this.btnWS2.TabStop = false;
             this.btnWS2.Text = "Web Service 2";
-            this.btnWS2.UseVisualStyleBackColor = true;
+            this.btnWS2.UseVisualStyleBackColor = false;
             this.btnWS2.Click += new System.EventHandler(this.btnWS2_Click);
             // 
             // btnERP
             // 
-            this.btnERP.Location = new System.Drawing.Point(30, 271);
+            this.btnERP.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(255)))), ((int)(((byte)(128)))), ((int)(((byte)(0)))));
+            this.btnERP.FlatAppearance.BorderColor = System.Drawing.Color.FromArgb(((int)(((byte)(0)))), ((int)(((byte)(255)))), ((int)(((byte)(255)))), ((int)(((byte)(255)))));
+            this.btnERP.FlatAppearance.BorderSize = 0;
+            this.btnERP.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.btnERP.Font = new System.Drawing.Font("Microsoft Sans Serif", 7.8F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnERP.ForeColor = System.Drawing.Color.White;
+            this.btnERP.Location = new System.Drawing.Point(40, 321);
+            this.btnERP.Margin = new System.Windows.Forms.Padding(4, 2, 4, 2);
             this.btnERP.Name = "btnERP";
-            this.btnERP.Size = new System.Drawing.Size(492, 23);
-            this.btnERP.TabIndex = 4;
-            this.btnERP.Text = "ERP ";
-            this.btnERP.UseVisualStyleBackColor = true;
+            this.btnERP.Size = new System.Drawing.Size(656, 38);
+            this.btnERP.TabIndex = 0;
+            this.btnERP.TabStop = false;
+            this.btnERP.Text = "ERP";
+            this.btnERP.UseVisualStyleBackColor = false;
             this.btnERP.Click += new System.EventHandler(this.btnERP_Click);
             // 
             // label1
             // 
             this.label1.AutoSize = true;
-            this.label1.Location = new System.Drawing.Point(219, 39);
+            this.label1.Font = new System.Drawing.Font("Arial Narrow", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.Location = new System.Drawing.Point(275, 49);
+            this.label1.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
             this.label1.Name = "label1";
-            this.label1.Size = new System.Drawing.Size(120, 13);
+            this.label1.Size = new System.Drawing.Size(188, 24);
             this.label1.TabIndex = 5;
-            this.label1.Text = "Chooese an assignment";
+            this.label1.Text = "Choose an assignment";
             // 
-            // Form1
+            // MainClient
             // 
-            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(552, 340);
+            this.ClientSize = new System.Drawing.Size(736, 418);
             this.Controls.Add(this.label1);
             this.Controls.Add(this.btnERP);
             this.Controls.Add(this.btnWS2);
             this.Controls.Add(this.btnWS1);
             this.Controls.Add(this.btnPK2);
             this.Controls.Add(this.btnPK1);
-            this.Name = "Form1";
-            this.Text = "Form1";
+            this.Margin = new System.Windows.Forms.Padding(4);
+            this.Name = "MainClient";
+            this.Text = "Home";
             this.ResumeLayout(false);
             this.PerformLayout();
 
