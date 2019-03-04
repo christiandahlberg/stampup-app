106fa8e61dbcb1c9e22fdaec5f56c48951a296cd
soren <43566132+sorenljunggren@users.noreply.github.com>
2019-03-04 14:05:39 +0100

    ERP WFC

---
+++ ERP1_WindowsForms_Client/Controller.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ERP1_WindowsForms_Client
+{
+    class Controller
+    {
+    }
+}
