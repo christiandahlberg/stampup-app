6214d506ece3772a88ae1b5eb2ee893160d6f7d6
Victor El Bishti <victorelbishti@gmail.com>
2019-03-04 10:04:37 +0100

    WS1

---
+++ WS1_WebService/ReadFile.asmx
@@ -0,0 +1,1 @@
+﻿<%@ WebService Language="C#" CodeBehind="ReadFile.asmx.cs" Class="WS1_WebService.ReadFile" %>
---
+++ WS1_WebService/ReadFile.asmx.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Web;
+using System.Web.Services;
+
+namespace WS1_WebService
+{
+    /// <summary>
+    /// Summary description for ReadFile
+    /// </summary>
+    [WebService(Namespace = "http://tempuri.org/")]
+    [WebServiceBinding(ConformsTo = WsiProfiles.BasicProfile1_1)]
+    [System.ComponentModel.ToolboxItem(false)]
+    // To allow this Web Service to be called from script, using ASP.NET AJAX, uncomment the following line. 
+    // [System.Web.Script.Services.ScriptService]
+    public class ReadFile : System.Web.Services.WebService
+    {
+
+        [WebMethod]
+        public string OpenFile(string filePath)
+        {
+
+            try
+            {   // Open the text file using a stream reader.
+                using (StreamReader sr = new StreamReader(filePath, Encoding.Default, true))
+                {
+                    // Read the stream to a string, and write the string to the console.
+                    return sr.ReadToEnd();
+                }
+            }
+            catch (IOException e)
+            {
+                return "The file could not be read:\n" + e.Message;
+            }
+        }
+    }
+}
