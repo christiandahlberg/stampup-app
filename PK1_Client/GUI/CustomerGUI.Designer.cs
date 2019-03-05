c414d19297ac432bf3cc94fd4a6f78ac005524e0
soren <43566132+sorenljunggren@users.noreply.github.com>
2019-03-05 13:46:18 +0100

    u

--- PK1_Client/GUI/CustomerGUI.Designer.cs
+++ PK1_Client/GUI/CustomerGUI.Designer.cs
@@ -64,10 +64,10 @@
             this.btn_CustomerLogout.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
             this.btn_CustomerLogout.Font = new System.Drawing.Font("Microsoft Sans Serif", 7.8F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
             this.btn_CustomerLogout.ForeColor = System.Drawing.Color.White;
-            this.btn_CustomerLogout.Location = new System.Drawing.Point(603, 406);
-            this.btn_CustomerLogout.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
+            this.btn_CustomerLogout.Location = new System.Drawing.Point(452, 330);
+            this.btn_CustomerLogout.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
             this.btn_CustomerLogout.Name = "btn_CustomerLogout";
-            this.btn_CustomerLogout.Size = new System.Drawing.Size(159, 34);
+            this.btn_CustomerLogout.Size = new System.Drawing.Size(119, 28);
             this.btn_CustomerLogout.TabIndex = 0;
             this.btn_CustomerLogout.TabStop = false;
             this.btn_CustomerLogout.Text = "LOG OUT";
@@ -78,9 +78,10 @@
             // 
             this.lbl_CustAvailableStores.AutoSize = true;
             this.lbl_CustAvailableStores.Font = new System.Drawing.Font("Microsoft Sans Serif", 7.8F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.lbl_CustAvailableStores.Location = new System.Drawing.Point(18, 26);
+            this.lbl_CustAvailableStores.Location = new System.Drawing.Point(14, 21);
+            this.lbl_CustAvailableStores.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
             this.lbl_CustAvailableStores.Name = "lbl_CustAvailableStores";
-            this.lbl_CustAvailableStores.Size = new System.Drawing.Size(55, 17);
+            this.lbl_CustAvailableStores.Size = new System.Drawing.Size(43, 13);
             this.lbl_CustAvailableStores.TabIndex = 3;
             this.lbl_CustAvailableStores.Text = "Stores";
             // 
@@ -88,15 +89,18 @@
             // 
             this.lbl_Subscriptions.AutoSize = true;
             this.lbl_Subscriptions.Font = new System.Drawing.Font("Microsoft Sans Serif", 7.8F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.lbl_Subscriptions.Location = new System.Drawing.Point(27, 14);
+            this.lbl_Subscriptions.Location = new System.Drawing.Point(20, 11);
+            this.lbl_Subscriptions.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
             this.lbl_Subscriptions.Name = "lbl_Subscriptions";
-            this.lbl_Subscriptions.Size = new System.Drawing.Size(98, 17);
+            this.lbl_Subscriptions.Size = new System.Drawing.Size(77, 13);
             this.lbl_Subscriptions.TabIndex = 4;
             this.lbl_Subscriptions.Text = "Subscription";
             // 
             // dgv_CustStores
             // 
             this.dgv_CustStores.AllowUserToAddRows = false;
+            this.dgv_CustStores.AllowUserToDeleteRows = false;
+            this.dgv_CustStores.AllowUserToResizeRows = false;
             this.dgv_CustStores.AutoSizeColumnsMode = System.Windows.Forms.DataGridViewAutoSizeColumnsMode.Fill;
             this.dgv_CustStores.BackgroundColor = System.Drawing.SystemColors.Control;
             this.dgv_CustStores.BorderStyle = System.Windows.Forms.BorderStyle.Fixed3D;
@@ -106,15 +110,15 @@
             this.c_StoreID,
             this.c_StoreName,
             this.c_StoreAddress});
-            this.dgv_CustStores.Location = new System.Drawing.Point(21, 60);
-            this.dgv_CustStores.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
+            this.dgv_CustStores.Location = new System.Drawing.Point(16, 49);
+            this.dgv_CustStores.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
             this.dgv_CustStores.Name = "dgv_CustStores";
             this.dgv_CustStores.ReadOnly = true;
             this.dgv_CustStores.RowHeadersVisible = false;
             this.dgv_CustStores.RowTemplate.DividerHeight = 2;
             this.dgv_CustStores.RowTemplate.Height = 24;
             this.dgv_CustStores.SelectionMode = System.Windows.Forms.DataGridViewSelectionMode.FullRowSelect;
-            this.dgv_CustStores.Size = new System.Drawing.Size(323, 202);
+            this.dgv_CustStores.Size = new System.Drawing.Size(242, 164);
             this.dgv_CustStores.TabIndex = 6;
             this.dgv_CustStores.CellClick += new System.Windows.Forms.DataGridViewCellEventHandler(this.ShowSubscriptions);
             // 
@@ -140,11 +144,10 @@
             // 
             // progressBar_Stamps
             // 
-            this.progressBar_Stamps.Location = new System.Drawing.Point(285, 53);
-            this.progressBar_Stamps.Margin = new System.Windows.Forms.Padding(4);
+            this.progressBar_Stamps.Location = new System.Drawing.Point(214, 43);
             this.progressBar_Stamps.Maximum = 10;
             this.progressBar_Stamps.Name = "progressBar_Stamps";
-            this.progressBar_Stamps.Size = new System.Drawing.Size(263, 43);
+            this.progressBar_Stamps.Size = new System.Drawing.Size(197, 35);
             this.progressBar_Stamps.TabIndex = 7;
             // 
             // panel_Subscriptions
@@ -159,39 +162,35 @@
             this.panel_Subscriptions.Controls.Add(this.lbl_PanelStoreName);
             this.panel_Subscriptions.Controls.Add(this.lbl_PanelStore);
             this.panel_Subscriptions.Controls.Add(this.progressBar_Stamps);
-            this.panel_Subscriptions.Location = new System.Drawing.Point(-7, 268);
-            this.panel_Subscriptions.Margin = new System.Windows.Forms.Padding(4);
+            this.panel_Subscriptions.Location = new System.Drawing.Point(-5, 218);
             this.panel_Subscriptions.Name = "panel_Subscriptions";
-            this.panel_Subscriptions.Size = new System.Drawing.Size(603, 188);
+            this.panel_Subscriptions.Size = new System.Drawing.Size(452, 153);
             this.panel_Subscriptions.TabIndex = 8;
             this.panel_Subscriptions.Visible = false;
             // 
             // lbl_PanelProgressNumber
             // 
             this.lbl_PanelProgressNumber.Font = new System.Drawing.Font("Microsoft Sans Serif", 8.25F, System.Drawing.FontStyle.Italic, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.lbl_PanelProgressNumber.Location = new System.Drawing.Point(380, 14);
-            this.lbl_PanelProgressNumber.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.lbl_PanelProgressNumber.Location = new System.Drawing.Point(285, 11);
             this.lbl_PanelProgressNumber.Name = "lbl_PanelProgressNumber";
-            this.lbl_PanelProgressNumber.Size = new System.Drawing.Size(215, 28);
+            this.lbl_PanelProgressNumber.Size = new System.Drawing.Size(161, 23);
             this.lbl_PanelProgressNumber.TabIndex = 17;
             // 
             // lbl_PanelProgress
             // 
             this.lbl_PanelProgress.AutoSize = true;
             this.lbl_PanelProgress.Font = new System.Drawing.Font("Microsoft Sans Serif", 8.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.lbl_PanelProgress.Location = new System.Drawing.Point(281, 14);
-            this.lbl_PanelProgress.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.lbl_PanelProgress.Location = new System.Drawing.Point(211, 11);
             this.lbl_PanelProgress.Name = "lbl_PanelProgress";
-            this.lbl_PanelProgress.Size = new System.Drawing.Size(73, 17);
+            this.lbl_PanelProgress.Size = new System.Drawing.Size(56, 13);
             this.lbl_PanelProgress.TabIndex = 16;
             this.lbl_PanelProgress.Text = "Progress";
             // 
             // lbl_PanelDescriptionText
             // 
-            this.lbl_PanelDescriptionText.Location = new System.Drawing.Point(133, 121);
-            this.lbl_PanelDescriptionText.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.lbl_PanelDescriptionText.Location = new System.Drawing.Point(100, 98);
             this.lbl_PanelDescriptionText.Name = "lbl_PanelDescriptionText";
-            this.lbl_PanelDescriptionText.Size = new System.Drawing.Size(415, 39);
+            this.lbl_PanelDescriptionText.Size = new System.Drawing.Size(311, 32);
             this.lbl_PanelDescriptionText.TabIndex = 13;
             this.lbl_PanelDescriptionText.Text = "Köp nio sallader och få den tionde gratis! Pangdeal! Bara hos Grönt och Gott.";
             this.lbl_PanelDescriptionText.TextAlign = System.Drawing.ContentAlignment.MiddleLeft;
@@ -199,10 +198,9 @@
             // lbl_PanelOfferName
             // 
             this.lbl_PanelOfferName.AutoSize = true;
-            this.lbl_PanelOfferName.Location = new System.Drawing.Point(133, 92);
-            this.lbl_PanelOfferName.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.lbl_PanelOfferName.Location = new System.Drawing.Point(100, 75);
             this.lbl_PanelOfferName.Name = "lbl_PanelOfferName";
-            this.lbl_PanelOfferName.Size = new System.Drawing.Size(63, 17);
+            this.lbl_PanelOfferName.Size = new System.Drawing.Size(48, 13);
             this.lbl_PanelOfferName.TabIndex = 12;
             this.lbl_PanelOfferName.Text = "Klippkort";
             // 
@@ -210,10 +208,9 @@
             // 
             this.lbl_PanelDescription.AutoSize = true;
             this.lbl_PanelDescription.Font = new System.Drawing.Font("Microsoft Sans Serif", 8.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.lbl_PanelDescription.Location = new System.Drawing.Point(25, 133);
-            this.lbl_PanelDescription.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.lbl_PanelDescription.Location = new System.Drawing.Point(19, 108);
             this.lbl_PanelDescription.Name = "lbl_PanelDescription";
-            this.lbl_PanelDescription.Size = new System.Drawing.Size(90, 17);
+            this.lbl_PanelDescription.Size = new System.Drawing.Size(71, 13);
             this.lbl_PanelDescription.TabIndex = 11;
             this.lbl_PanelDescription.Text = "Description";
             // 
@@ -221,10 +218,9 @@
             // 
             this.lbl_PanelOffer.AutoSize = true;
             this.lbl_PanelOffer.Font = new System.Drawing.Font("Microsoft Sans Serif", 8.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.lbl_PanelOffer.Location = new System.Drawing.Point(25, 92);
-            this.lbl_PanelOffer.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.lbl_PanelOffer.Location = new System.Drawing.Point(19, 75);
             this.lbl_PanelOffer.Name = "lbl_PanelOffer";
-            this.lbl_PanelOffer.Size = new System.Drawing.Size(45, 17);
+            this.lbl_PanelOffer.Size = new System.Drawing.Size(35, 13);
             this.lbl_PanelOffer.TabIndex = 10;
             this.lbl_PanelOffer.Text = "Offer";
             // 
@@ -232,10 +228,9 @@
             // 
             this.lbl_PanelStoreName.AutoSize = true;
             this.lbl_PanelStoreName.Font = new System.Drawing.Font("Microsoft Sans Serif", 8.25F, System.Drawing.FontStyle.Italic, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.lbl_PanelStoreName.Location = new System.Drawing.Point(129, 53);
-            this.lbl_PanelStoreName.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.lbl_PanelStoreName.Location = new System.Drawing.Point(97, 43);
             this.lbl_PanelStoreName.Name = "lbl_PanelStoreName";
-            this.lbl_PanelStoreName.Size = new System.Drawing.Size(102, 17);
+            this.lbl_PanelStoreName.Size = new System.Drawing.Size(77, 13);
             this.lbl_PanelStoreName.TabIndex = 9;
             this.lbl_PanelStoreName.Text = "Grönt och Gott";
             // 
@@ -243,16 +238,17 @@
             // 
             this.lbl_PanelStore.AutoSize = true;
             this.lbl_PanelStore.Font = new System.Drawing.Font("Microsoft Sans Serif", 8.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.lbl_PanelStore.Location = new System.Drawing.Point(25, 53);
-            this.lbl_PanelStore.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.lbl_PanelStore.Location = new System.Drawing.Point(19, 43);
             this.lbl_PanelStore.Name = "lbl_PanelStore";
-            this.lbl_PanelStore.Size = new System.Drawing.Size(47, 17);
+            this.lbl_PanelStore.Size = new System.Drawing.Size(37, 13);
             this.lbl_PanelStore.TabIndex = 8;
             this.lbl_PanelStore.Text = "Store";
             // 
             // dgv_Offers
             // 
             this.dgv_Offers.AllowUserToAddRows = false;
+            this.dgv_Offers.AllowUserToDeleteRows = false;
+            this.dgv_Offers.AllowUserToResizeRows = false;
             this.dgv_Offers.AutoSizeColumnsMode = System.Windows.Forms.DataGridViewAutoSizeColumnsMode.Fill;
             this.dgv_Offers.BackgroundColor = System.Drawing.SystemColors.Control;
             this.dgv_Offers.BorderStyle = System.Windows.Forms.BorderStyle.Fixed3D;
@@ -263,15 +259,15 @@
             this.c_OfferName,
             this.c_OfferCreated,
             this.c_CustomerStoreId});
-            this.dgv_Offers.Location = new System.Drawing.Point(371, 60);
-            this.dgv_Offers.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
+            this.dgv_Offers.Location = new System.Drawing.Point(278, 49);
+            this.dgv_Offers.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
             this.dgv_Offers.Name = "dgv_Offers";
             this.dgv_Offers.ReadOnly = true;
             this.dgv_Offers.RowHeadersVisible = false;
             this.dgv_Offers.RowTemplate.DividerHeight = 2;
             this.dgv_Offers.RowTemplate.Height = 24;
             this.dgv_Offers.SelectionMode = System.Windows.Forms.DataGridViewSelectionMode.FullRowSelect;
-            this.dgv_Offers.Size = new System.Drawing.Size(404, 202);
+            this.dgv_Offers.Size = new System.Drawing.Size(303, 164);
             this.dgv_Offers.TabIndex = 9;
             this.dgv_Offers.CellClick += new System.Windows.Forms.DataGridViewCellEventHandler(this.ShowOfferProgress);
             // 
@@ -308,25 +304,24 @@
             // 
             this.lbl_CustomerOffers.AutoSize = true;
             this.lbl_CustomerOffers.Font = new System.Drawing.Font("Microsoft Sans Serif", 8.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.lbl_CustomerOffers.Location = new System.Drawing.Point(371, 26);
-            this.lbl_CustomerOffers.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.lbl_CustomerOffers.Location = new System.Drawing.Point(278, 21);
             this.lbl_CustomerOffers.Name = "lbl_CustomerOffers";
-            this.lbl_CustomerOffers.Size = new System.Drawing.Size(53, 17);
+            this.lbl_CustomerOffers.Size = new System.Drawing.Size(41, 13);
             this.lbl_CustomerOffers.TabIndex = 10;
             this.lbl_CustomerOffers.Text = "Offers";
             // 
             // CustomerGUI
             // 
-            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(800, 460);
+            this.ClientSize = new System.Drawing.Size(600, 374);
             this.Controls.Add(this.btn_CustomerLogout);
             this.Controls.Add(this.lbl_CustomerOffers);
             this.Controls.Add(this.dgv_Offers);
             this.Controls.Add(this.panel_Subscriptions);
             this.Controls.Add(this.dgv_CustStores);
             this.Controls.Add(this.lbl_CustAvailableStores);
-            this.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
+            this.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
             this.Name = "CustomerGUI";
             this.Text = "Stämpelkortify";
             ((System.ComponentModel.ISupportInitialize)(this.dgv_CustStores)).EndInit();
